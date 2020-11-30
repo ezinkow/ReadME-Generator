e77e9c37d22f03a9d39b0e2ab1980cec921c16a1
Elan Zinkow <ezinkow@gmail.com>
2020-11-30 17:33:11 -0600

    working node and output

--- README.md
+++ README.md
@@ -1,13 +1,28 @@
 
-    ## alskdfsdf
-    ##Installation instructions:asdfasdf
-    ##Link to Website:<br>asdasdlksadg
-    ##Usage Instructions:<br>asldkgasdlg
-    ##About the Project:<br>aslkdgsdg
-    ##Summary of Project:<br>sa
-    ##Created by:<br>dgasdgasdg
-    ##Collaborators:<br>salkdgsdg
-    ##Licensing information:<br>lkasdf
-    
-    
-    ## name
+# ReadME generator
+## Description:
+makes a readme
+Link to website: [google.com](google.com)
+## Table of Contents
+
+* [Installation](#installation)
+* [Usage](#usage)
+* [Credits](#credits)
+* [License](#license)
+* [Contributing](#contributing)
+* [Tests](#tests)
+* [Questions](#questions)
+
+## Installation:
+laksdf
+## Usage:
+asldkfasd
+## License:
+flaskdfa
+## Contributing:
+sdflaksdf
+## Tests:
+lakshdf
+## Questions:
+You may contact Elan at ezinkow@gmail.com
+And be sure to check out my [GitHub page](github.com/ezinkow)
--- index.js
+++ index.js
@@ -20,9 +20,7 @@ const fs = require('fs');
 // init();
 
 
-// fs.writeFile('ReadMe.md', process.argv[2], (err) =>
-//     err ? console.error(err) : console.log('ReadME Created!')
-// );
+//Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
 
 inquirer
     .prompt([
@@ -33,8 +31,8 @@ inquirer
         },
         {
             type: 'input',
-            message: 'What are the steps to install the app/project?',
-            name: 'installation',
+            message: 'Please describe the project:',
+            name: 'description',
         },
         {
             type: 'input',
@@ -43,56 +41,89 @@ inquirer
         },
         {
             type: 'input',
+            message: 'What are the steps to install the app/project?',
+            name: 'installation',
+        },
+        {
+            type: 'input',
             message: 'What are the usage instructions?',
             name: 'usage',
-        },
-        {
-            type: 'input',
-            message: 'About the project:',
-            name: 'about',
-        },
-        {
-            type: 'input',
-            message: 'Summary of project:',
-            name: 'summary',
-        },
-        {
-            type: 'input',
-            message: 'Created by:',
-            name: 'creator',
-        },
-        {
-            type: 'input',
-            message: 'Are there collaborators on this project? Please list them including GitHub usernames:',
-            name: 'collaborators',
         },
         {
             type: 'input',
             message: 'Please give any license information:',
             name: 'license',
         },
+        {
+            type: 'input',
+            message: 'How can users contribute to your project?',
+            name: 'contributing',
+        },
+        {
+            type: 'input',
+            message: 'How can users test the application?',
+            name: 'tests',
+        },
+        {
+            type: 'input',
+            message: 'What is your name, or name of the creator of the website?',
+            name: 'name',
+        },
+        {
+            type: 'input',
+            message: 'What is your email?',
+            name: 'email',
+        },
+        {
+            type: 'input',
+            message: 'What is your GitHub username?',
+            name: 'github',
+        },
+
+       
     ])
 
-    .then((response) => {
-        const mdOutput = `
-    #${response.title}
-    ##Installation instructions:${response.installation}
-    ##Link to Website:<br>${response.link}
-    ##Usage Instructions:<br>${response.usage}
-    ##About the Project:<br>${response.about}
-    ##Summary of Project:<br>${response.summary}
-    ##Created by:<br>${response.creator}
-    ##Collaborators:<br>${response.collaborators}
-    ##Licensing information:<br>${response.license}
-    
-    `;
+    //Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
+
+.then((response) => {
+    const mdOutput = `
+# ${response.title}
+## Description:
+${response.description}
+Link to website: [${response.link}](${response.link})
+## Table of Contents
+
+* [Installation](#installation)
+* [Usage](#usage)
+* [License](#license)
+* [Contributing](#contributing)
+* [Tests](#tests)
+* [Questions](#questions)
+
+## Installation:
+${response.installation}
+## Usage:
+${response.usage}
+## License:
+${response.license}
+## Contributing:
+${response.contributing}
+## Tests:
+${response.tests}
+## Questions:
+You may contact ${response.name} at ${response.email}
+And be sure to check out my [GitHub page](github.com/${response.github})
+`;
         console.log('response:', response);
 
-        fs.writeFile('ReadME.md', mdOutput, (err) =>
-            err
-                ? console.error(err)
-                : console.log('ReadME created successfully!')
-        );
+fs.writeFile('ReadME.md', mdOutput, (err) =>
+    err
+    ? console.error(err)
+    : console.log('ReadME created successfully!')
+);
 
     });
 
+// fs.writeFile('ReadMe.md', process.argv[2], (err) =>
+//     err ? console.error(err) : console.log('ReadME Created!')
+// );
